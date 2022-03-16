d5c295b239c2a43bd192f8ba8d804997e038f1b2
Gabriel Souki <gabrielsouki@gmail.com>
2022-03-16 17:03:36 -0400

    El cohete ahora cuenta con movimiento y detección de colisiones

---
+++ Assets/Scripts/Proyectile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectile : MonoBehaviour
+{
+    public float proyectileSpeed;
+    float currentProyectileSpeed;
+
+    Rigidbody2D m_ridigBody;
+
+    public bool facingRight = false;
+
+    Vector3 startPosition;
+
+
+    private void Awake()
+    {
+        m_ridigBody = GetComponent<Rigidbody2D>();
+        startPosition = this.transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = startPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        SetProyectileDirection();
+        ProyectileMovement();
+    }
+
+    void SetProyectileDirection()
+    {
+        if (facingRight)
+        {
+            currentProyectileSpeed = proyectileSpeed;
+            this.transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            currentProyectileSpeed = -proyectileSpeed;
+            this.transform.eulerAngles = Vector3.zero;
+        }
+    }
+
+    void ProyectileMovement()
+    {
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            m_ridigBody.velocity = Vector2.right * currentProyectileSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/RocketEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketEnemy : MonoBehaviour
+{
+    int rocketDamage = 10;
+    Proyectile m_proyectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_proyectile = this.gameObject.GetComponent<Proyectile>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Player": collision.GetComponent<PlayerController>().CollectHealth(-rocketDamage);
+                break;
+            case "Coin":
+                break;
+            default: m_proyectile.facingRight = !m_proyectile.facingRight;
+                break;
+        }
+    }
+}
