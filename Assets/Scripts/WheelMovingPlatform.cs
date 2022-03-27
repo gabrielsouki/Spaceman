4671a50d21492f8d51512fad96b38f34db52e567
Gabriel Souki <gabrielsouki@gmail.com>
2022-03-26 23:36:49 -0400

    Se arreglo el problema de las plataformas giratorias

--- Assets/Scripts/WheelMovingPlatform.cs
+++ Assets/Scripts/WheelMovingPlatform.cs
@@ -6,9 +6,20 @@ public class WheelMovingPlatform : MonoBehaviour
 {
     [SerializeField]
     float rotationSpeed;
+    PauseMenu m_pauseMenu;
+
+    private void Start()
+    {
+        m_pauseMenu = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PauseMenu>();
+    }
+
     void Update()
     {
-        transform.RotateAround(this.transform.parent.position, Vector3.back, rotationSpeed);
-        transform.up = Vector3.up;
+        if (!m_pauseMenu.isPaused)
+        {
+            transform.RotateAround(this.transform.parent.position, Vector3.back, rotationSpeed);
+            transform.up = Vector3.up;
+        }
+        
     }
 }
